bec822e86e603f06847cdcb985b8984e997063b0
Andrzej Kaczmarek <andrzej.kaczmarek@tieto.com>
2014-03-19 12:08:08 +0100

    Enable HFP WBS by default
    Change-Id: I31727292772231c8ebfb217b23af0cdde235ed0c

--- device.mk
+++ device.mk
@@ -272,5 +272,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	persist.sys.usb.config=mtp
 
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.sys.bluetooth.handsfree=hfp_wbs
+
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
